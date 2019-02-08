64acac49e5e6c4372928eab2bd96482cb27d38c8
RicardoHernandez96 <ricardo.hernandez@coalmx.com>
2019-02-08 11:05:44 -0600

    componentes de panel de control y login

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { PanelComponent } from './panel/panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent,
+    PanelComponent
   ],
   imports: [
     BrowserModule,
