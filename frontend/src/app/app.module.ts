854e36538bb72b3f41b82c994622d9433d59ffae
RicardoHernandez96 <ricardo.hernandez@coalmx.com>
2019-02-11 18:29:51 -0600

    login y recuperar contraseña terminados

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,19 +6,22 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import { PanelComponent } from './panel/panel.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LoginComponent,
-    PanelComponent
+    PanelComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [HeaderComponent]
+  bootstrap: [ForgotPasswordComponent]
 })
 export class AppModule { }
