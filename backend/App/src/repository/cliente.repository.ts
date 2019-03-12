4c859041953cc276127282f2c781b66ed58068d3
GerardoZamudio58 <gerardo.zamudio@coalmx.com>
2019-03-11 18:34:10 -0600

    Documentacion Plantilla

--- backend/App/src/repository/cliente.repository.ts
+++ backend/App/src/repository/cliente.repository.ts
@@ -81,6 +81,10 @@ export class ClienteRepository {
         return this.query.spExecute(query, "[cliente].[SEL_CLIENTEENTIDADCONDIRECCION_SP]")
     }
 
+    getTipoDocumento(query: any): PromiseLike<{}> {
+        return this.query.spExecute(query, "[cliente].[SEL_TIPODOCUMENTO_SP]")
+    }
+
     // ************ Servicios POST ************
 
     /**
